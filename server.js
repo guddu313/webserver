b1bc144e4cadacc05a14305928290918838aaa42
Pravin Gareta <pravin.gareta@einfochips.com>
2017-07-04 18:07:45 +0530

    Initialize repo

---
+++ server.js
@@ -0,0 +1,33 @@
+var server = require('express');
+var port = 3000;
+
+var app = server();
+
+
+//To get the HTML request and response
+/*app.get('/',function(req, res){
+		res.send('Hello Gareta');
+});*/
+
+var middleware = {
+		requireAuthentication: function(req, res, next){
+				console.log('private route hit!');
+				next();
+		},
+		logger: function(req, res, next){
+				console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+				next();
+		}
+}
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+		res.send('About Us');
+});
+
+app.use(server.static(__dirname + '/public'));
+
+app.listen(port, function(){
+	console.log('Server started...' + 'Port : ' + port);
+});
